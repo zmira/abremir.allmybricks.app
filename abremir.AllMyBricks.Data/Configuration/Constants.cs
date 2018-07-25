09f84e3f87f13232301751264cdffdebf8794911
José Mira <jose.rui.mira@gmail.com>
2018-07-25 23:49:08 +0100

    rename database file name extension

--- abremir.AllMyBricks.Data/Configuration/Constants.cs
+++ abremir.AllMyBricks.Data/Configuration/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string AllMyBricksDbFile = "allmybricks.litedb";
+        public const string AllMyBricksDbFile = "allmybricks.realm";
         public const ushort MinimumSetYear = 1949;
         public const ushort MinimumSearchQuerySize = 3;
     }
