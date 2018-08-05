25ae970a8b95eb17ab1491c025bf592c5db7f697
José Mira <jose.rui.mira@gmail.com>
2018-08-05 02:19:26 +0100

    add tests for Theme Data Synchronizer

---
+++ abremir.AllMyBricks.DataSynchronizer.Tests/Configuration/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace abremir.AllMyBricks.DataSynchronizer.Tests.Configuration
+{
+    public static class Constants
+    {
+        public const string JsonFileGetThemes = "GetThemes";
+        public const string JsonFileGetYears = "GetYears";
+
+        public const string TestTheme = "Architecture";
+    }
+}
--- abremir.AllMyBricks.DataSynchronizer.Tests/Synchronizers/ThemeSynchronizerTests.cs
+++ abremir.AllMyBricks.DataSynchronizer.Tests/Synchronizers/ThemeSynchronizerTests.cs
@@ -1,6 +1,75 @@
-﻿namespace abremir.AllMyBricks.DataSynchronizer.Tests.Synchronizers
+﻿using abremir.AllMyBricks.Data.Interfaces;
+using abremir.AllMyBricks.Data.Repositories;
+using abremir.AllMyBricks.DataSynchronizer.Synchronizers;
+using abremir.AllMyBricks.DataSynchronizer.Tests.Configuration;
+using abremir.AllMyBricks.DataSynchronizer.Tests.Shared;
+using abremir.AllMyBricks.ThirdParty.Brickset.Interfaces;
+using abremir.AllMyBricks.ThirdParty.Brickset.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace abremir.AllMyBricks.DataSynchronizer.Tests.Synchronizers
 {
-    public class ThemeSynchronizerTests
+    [TestClass]
+    public class ThemeSynchronizerTests : DataSynchronizerTestsBase
     {
+        private static IThemeRepository _themeRepository;
+
+        [ClassInitialize]
+#pragma warning disable RCS1163 // Unused parameter.
+        public static void ClassInitialize(TestContext testContext)
+#pragma warning restore RCS1163 // Unused parameter.
+        {
+            _themeRepository = new ThemeRepository(MemoryRepositoryService);
+        }
+
+        [TestMethod]
+        public void Synchronize_BricksetApiServiceReturnsEmptyList_NothingIsSaved()
+        {
+            var bricksetApiService = Substitute.For<IBricksetApiService>();
+            bricksetApiService
+                .GetThemes(Arg.Any<ParameterApiKey>())
+                .Returns(Enumerable.Empty<Themes>());
+
+            var themeSynchronizer = CreateTarget(bricksetApiService);
+
+            var themes = themeSynchronizer.Synchronize(string.Empty);
+
+            themes.Should().BeEmpty();
+            _themeRepository.All().Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void Synchronize_BricksetApiServiceReturnsListOfThemes_AllThemesAreSaved()
+        {
+            var themesList = fastJSON.JSON.ToObject<List<Themes>>(GetResultFileFromResource(Constants.JsonFileGetThemes));
+            var yearsList = fastJSON.JSON.ToObject<List<Years>>(GetResultFileFromResource(Constants.JsonFileGetYears));
+
+            var bricksetApiService = Substitute.For<IBricksetApiService>();
+            bricksetApiService
+                .GetThemes(Arg.Any<ParameterApiKey>())
+                .Returns(themesList);
+            bricksetApiService
+                .GetYears(Arg.Is<ParameterTheme>(parameter => parameter.Theme == Constants.TestTheme))
+                .Returns(yearsList);
+
+            var themeSynchronizer = CreateTarget(bricksetApiService);
+
+            var themes = themeSynchronizer.Synchronize(string.Empty);
+
+            themes.Count().Should().Be(themesList.Count);
+            _themeRepository.All().Count().Should().Be(themesList.Count);
+            _themeRepository.Get(Constants.TestTheme).SetCountPerYear.Should().NotBeEmpty();
+        }
+
+        private ThemeSynchronizer CreateTarget(IBricksetApiService bricksetApiService = null)
+        {
+            bricksetApiService = bricksetApiService ?? Substitute.For<IBricksetApiService>();
+
+            return new ThemeSynchronizer(bricksetApiService, _themeRepository);
+        }
     }
 }
