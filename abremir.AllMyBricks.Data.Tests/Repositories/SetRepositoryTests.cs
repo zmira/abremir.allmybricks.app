aa66276142dd26c18082c7995709961f0b601fac
José Mira <jose.rui.mira@gmail.com>
2018-08-11 20:55:43 +0100

    disable extra warnings

--- abremir.AllMyBricks.Data.Tests/Repositories/SetRepositoryTests.cs
+++ abremir.AllMyBricks.Data.Tests/Repositories/SetRepositoryTests.cs
@@ -21,7 +21,9 @@ namespace abremir.AllMyBricks.Data.Tests.Repositories
 
         [ClassInitialize]
 #pragma warning disable RCS1163 // Unused parameter.
+#pragma warning disable RECS0154 // Parameter is never used
         public static void ClassInitialize(TestContext testContext)
+#pragma warning restore RECS0154 // Parameter is never used
 #pragma warning restore RCS1163 // Unused parameter.
         {
             _setRepository = new SetRepository(MemoryRepositoryService);
--- abremir.AllMyBricks.DataSynchronizer.Tests/Synchronizers/SetSynchronizerTests.cs
+++ abremir.AllMyBricks.DataSynchronizer.Tests/Synchronizers/SetSynchronizerTests.cs
@@ -26,7 +26,9 @@ namespace abremir.AllMyBricks.DataSynchronizer.Tests.Synchronizers
 
         [ClassInitialize]
 #pragma warning disable RCS1163 // Unused parameter.
+#pragma warning disable RECS0154 // Parameter is never used
         public static void ClassInitialize(TestContext testContext)
+#pragma warning restore RECS0154 // Parameter is never used
 #pragma warning restore RCS1163 // Unused parameter.
         {
             _setRepository = new SetRepository(MemoryRepositoryService);
