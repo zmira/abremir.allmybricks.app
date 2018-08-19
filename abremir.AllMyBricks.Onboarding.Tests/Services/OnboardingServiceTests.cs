76193420b8277a638adfbbaaa91d08fddc6f23cb
José Mira <jose.rui.mira@gmail.com>
2018-08-19 02:51:14 +0100

    add skeleton file for OnboardingServiceTests

---
+++ abremir.AllMyBricks.Onboarding.Tests/Services/OnboardingServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace abremir.AllMyBricks.Onboarding.Tests.Services
+{
+    [TestClass]
+    public class OnboardingServiceTests
+    {
+
+        [ClassInitialize]
+#pragma warning disable RCS1163 // Unused parameter.
+#pragma warning disable RECS0154 // Parameter is never used
+        public static void ClassInitialize(TestContext testContext)
+#pragma warning restore RECS0154 // Parameter is never used
+#pragma warning restore RCS1163 // Unused parameter.
+        {
+        }
+    }
+}
