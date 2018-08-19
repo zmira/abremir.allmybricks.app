5a07bbb3eb9b4a8f1d61273f78aa5e6fe2e651c2
José Mira <jose.rui.mira@gmail.com>
2018-08-19 01:19:58 +0100

    add OnboardingService which handle the retrieval of Brickset's API key: if app does not have it, request it from the Onboarding backend service.

--- abremir.AllMyBricks.Onboarding/Services/OnboardingService.cs
+++ abremir.AllMyBricks.Onboarding/Services/OnboardingService.cs
@@ -1,12 +1,61 @@
-﻿using abremir.AllMyBricks.Onboarding.Interfaces;
+﻿using abremir.AllMyBricks.Core.Models;
+using abremir.AllMyBricks.Device.Interfaces;
+using abremir.AllMyBricks.Onboarding.Interfaces;
 
 namespace abremir.AllMyBricks.Onboarding.Services
 {
     public class OnboardingService : IOnboardingService
     {
+        private readonly ISecureStorageService _secureStorageService;
+        private readonly IRegistrationService _registrationService;
+        private readonly IApiKeyService _apiKeyService;
+        private readonly IDeviceInformationService _deviceInformationService;
+
+        public OnboardingService(
+            ISecureStorageService secureStorageService,
+            IRegistrationService registrationService,
+            IApiKeyService apiKeyService,
+            IDeviceInformationService deviceInformationService)
+        {
+            _secureStorageService = secureStorageService;
+            _registrationService = registrationService;
+            _apiKeyService = apiKeyService;
+            _deviceInformationService = deviceInformationService;
+        }
+
         public string GetBricksetApiKey()
         {
-            throw new System.NotImplementedException();
+            var bricksetApiKey = _secureStorageService.GetBricksetApiKey();
+
+            if (string.IsNullOrWhiteSpace(bricksetApiKey))
+            {
+                bricksetApiKey = GetApiKeyFromOnboardingServiceEndpoint();
+
+                _secureStorageService.SaveBricksetApiKey(bricksetApiKey);
+            }
+
+            return bricksetApiKey;
+        }
+
+        private string GetApiKeyFromOnboardingServiceEndpoint()
+        {
+            Identification identification = null;
+
+            if (!_secureStorageService.DeviceIdentificationCreated)
+            {
+                identification = _registrationService.Register(new Identification
+                {
+                    DeviceIdentification = _deviceInformationService.GenerateNewDeviceIdentification()
+                });
+
+                _secureStorageService.SaveDeviceIdentification(identification);
+            }
+            else
+            {
+                identification = _secureStorageService.GetDeviceIdentification();
+            }
+
+            return _apiKeyService.GetBricksetApiKey(identification);
         }
     }
 }
