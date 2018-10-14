9d19c4e3d5951cd515205ea7523910c2a96a1865
José Mira <jose.rui.mira@gmail.com>
2018-10-14 01:29:26 +0100

    apparently, Brickset tags contain presentation logic in the value itself! add read-only properties to check tag type and access the tag's real value.

--- abremir.AllMyBricks.Data/Models/Tag.cs
+++ abremir.AllMyBricks.Data/Models/Tag.cs
@@ -1,9 +1,14 @@
 ﻿using abremir.AllMyBricks.Data.Interfaces;
+using System;
 
 namespace abremir.AllMyBricks.Data.Models
 {
     public class Tag: IReferenceData
     {
         public string Value { get; set; }
+
+        public bool IsName => Value.EndsWith("|n", StringComparison.InvariantCultureIgnoreCase);
+
+        public string TagValue => Value.Replace("|n", "");
     }
 }
