1b273449fea9fbbebd8a018c5111bd8b8807753f
José Mira <jose.rui.mira@gmail.com>
2018-10-14 01:37:58 +0100

    check-in missing string sanitization extension

---
+++ abremir.AllMyBricks.Data/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace abremir.AllMyBricks.Data.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Sanitize(this string source)
+        {
+            return source.Replace("'", "\\'").Trim();
+        }
+    }
+}
