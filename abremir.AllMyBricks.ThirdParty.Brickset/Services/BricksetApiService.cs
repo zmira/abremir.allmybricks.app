78d0ab0e74c139d4ff2cb791f4ec2fb9a1c71663
José Mira <jose.rui.mira@gmail.com>
2019-08-03 01:09:59 +0100

    rename private method

--- abremir.AllMyBricks.ThirdParty.Brickset/Services/BricksetApiService.cs
+++ abremir.AllMyBricks.ThirdParty.Brickset/Services/BricksetApiService.cs
@@ -15,65 +15,65 @@ namespace abremir.AllMyBricks.ThirdParty.Brickset.Services
     {
         public async Task<bool> CheckKey(ParameterApiKey checkKeyParameters)
         {
-            return (await GetBricksetData<ResultCheckKey, ResultCheckKey, ParameterApiKey>(checkKeyParameters)).Value.Equals(Constants.ResponseOk);
+            return (await BricksetHttpPostUrlEncodeAsync<ResultCheckKey, ResultCheckKey, ParameterApiKey>(checkKeyParameters)).Value.Equals(Constants.ResponseOk);
         }
 
         public async Task<string> Login(ParameterLogin loginParameters)
         {
-            return (await GetBricksetData<ResultLogin, ResultLogin, ParameterLogin>(loginParameters)).Value;
+            return (await BricksetHttpPostUrlEncodeAsync<ResultLogin, ResultLogin, ParameterLogin>(loginParameters)).Value;
         }
 
         public async Task<string> CheckUserHash(ParameterUserHash checkUserHashParameters)
         {
-            return (await GetBricksetData<ResultCheckUserHash, ResultCheckUserHash, ParameterUserHash>(checkUserHashParameters)).Value;
+            return (await BricksetHttpPostUrlEncodeAsync<ResultCheckUserHash, ResultCheckUserHash, ParameterUserHash>(checkUserHashParameters)).Value;
         }
 
         public async Task<IEnumerable<Themes>> GetThemes(ParameterApiKey getThemesParameters)
         {
-            return await GetBricksetData<ArrayOfThemes, IEnumerable<Themes>, ParameterApiKey>(getThemesParameters);
+            return await BricksetHttpPostUrlEncodeAsync<ArrayOfThemes, IEnumerable<Themes>, ParameterApiKey>(getThemesParameters);
         }
 
         public async Task<IEnumerable<Subthemes>> GetSubthemes(ParameterTheme getSubthemesParameters)
         {
-            return await GetBricksetData<ArrayOfSubthemes, IEnumerable<Subthemes>, ParameterTheme>(getSubthemesParameters);
+            return await BricksetHttpPostUrlEncodeAsync<ArrayOfSubthemes, IEnumerable<Subthemes>, ParameterTheme>(getSubthemesParameters);
         }
 
         public async Task<IEnumerable<Years>> GetYears(ParameterTheme getYearsParameters)
         {
-            return await GetBricksetData<ArrayOfYears, IEnumerable<Years>, ParameterTheme>(getYearsParameters);
+            return await BricksetHttpPostUrlEncodeAsync<ArrayOfYears, IEnumerable<Years>, ParameterTheme>(getYearsParameters);
         }
 
         public async Task<IEnumerable<Sets>> GetSets(ParameterSets getSetsParameters)
         {
-            return await GetBricksetData<ArrayOfSets, IEnumerable<Sets>, ParameterSets>(getSetsParameters);
+            return await BricksetHttpPostUrlEncodeAsync<ArrayOfSets, IEnumerable<Sets>, ParameterSets>(getSetsParameters);
         }
 
         public async Task<Sets> GetSet(ParameterUserHashSetId getSetParameters)
         {
-            return (await GetBricksetData<ArrayOfSet, IEnumerable<Sets>, ParameterUserHashSetId>(getSetParameters)).FirstOrDefault();
+            return (await BricksetHttpPostUrlEncodeAsync<ArrayOfSet, IEnumerable<Sets>, ParameterUserHashSetId>(getSetParameters)).FirstOrDefault();
         }
 
         public async Task<IEnumerable<Sets>> GetRecentlyUpdatedSets(ParameterMinutesAgo getRecentlyUpdatedSetsParameters)
         {
-            return await GetBricksetData<ArrayOfRecentlyUpdatedSets, IEnumerable<Sets>, ParameterMinutesAgo>(getRecentlyUpdatedSetsParameters);
+            return await BricksetHttpPostUrlEncodeAsync<ArrayOfRecentlyUpdatedSets, IEnumerable<Sets>, ParameterMinutesAgo>(getRecentlyUpdatedSetsParameters);
         }
 
         public async Task<IEnumerable<Instructions>> GetInstructions(ParameterSetId getInstructionsParameters)
         {
-            return await GetBricksetData<ArrayOfInstructions, IEnumerable<Instructions>, ParameterSetId>(getInstructionsParameters);
+            return await BricksetHttpPostUrlEncodeAsync<ArrayOfInstructions, IEnumerable<Instructions>, ParameterSetId>(getInstructionsParameters);
         }
 
         public async Task<IEnumerable<AdditionalImages>> GetAdditionalImages(ParameterSetId getAdditionalImagesParameters)
         {
-            return await GetBricksetData<ArrayOfAdditionalImages, IEnumerable<AdditionalImages>, ParameterSetId>(getAdditionalImagesParameters);
+            return await BricksetHttpPostUrlEncodeAsync<ArrayOfAdditionalImages, IEnumerable<AdditionalImages>, ParameterSetId>(getAdditionalImagesParameters);
         }
 
         public async Task<IEnumerable<Reviews>> GetReviews(ParameterSetId getReviewsParameters)
         {
-            return await GetBricksetData<ArrayOfReviews, IEnumerable<Reviews>, ParameterSetId>(getReviewsParameters);
+            return await BricksetHttpPostUrlEncodeAsync<ArrayOfReviews, IEnumerable<Reviews>, ParameterSetId>(getReviewsParameters);
         }
 
-        private async Task<U> GetBricksetData<T, U, V>(V parameters) where T: class where U : class where V : class
+        private async Task<U> BricksetHttpPostUrlEncodeAsync<T, U, V>(V parameters) where T: class where U : class where V : class
         {
             return await Constants.BricksetApiUrl.AppendPathSegment(typeof(T).GetDescription()).PostUrlEncodedAsync(parameters).ReceiveXml<T>() as U;
         }
