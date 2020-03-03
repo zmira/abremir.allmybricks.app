9869fc2cd37e45bdb675ce86de5a79024a052504
José Mira <jose.rui.mira@gmail.com>
2020-03-03 22:46:49 +0000

    add another pragma disable/restore around the ClassInitialize static method

--- abremir.AllMyBricks.ThirdParty.Brickset.Tests/Services/BricksetApiServiceTestsSetCollection.cs
+++ abremir.AllMyBricks.ThirdParty.Brickset.Tests/Services/BricksetApiServiceTestsSetCollection.cs
@@ -19,7 +19,9 @@ namespace abremir.AllMyBricks.ThirdParty.Brickset.Tests.Services
         [ClassInitialize]
 #pragma warning disable RCS1163 // Unused parameter.
 #pragma warning disable RECS0154 // Parameter is never used
+#pragma warning disable IDE0060 // Remove unused parameter
         public static void ClassInitialize(TestContext testContext)
+#pragma warning restore IDE0060 // Remove unused parameter
 #pragma warning restore RECS0154 // Parameter is never used
 #pragma warning restore RCS1163 // Unused parameter.
         {
