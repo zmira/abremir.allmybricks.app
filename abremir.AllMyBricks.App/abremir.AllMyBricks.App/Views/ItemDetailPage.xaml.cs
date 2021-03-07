99a6d69787237cd75c6ab85b4b02eb42d4cf2830
José Mira <jose.rui.mira@gmail.com>
2021-03-07 01:23:00 +0000

    replace existing skeleton android, ios, and uwp with updated versions and location within the solution folders

---
+++ abremir.AllMyBricks.App/abremir.AllMyBricks.App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using abremir.AllMyBricks.App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace abremir.AllMyBricks.App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
